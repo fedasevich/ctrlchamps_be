76eb81464304fb867ca9e60b951c12e638c0ab7b
ProggerOleg <olegtar25@gmail.com>
2023-12-07 21:26:27 +0200

    removed unused import

--- src/modules/virtual_assessment/virtual_assessment.module.ts
+++ src/modules/virtual_assessment/virtual_assessment.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Appointment } from 'src/common/entities/appointment.entity';
@@ -10,16 +8,7 @@ import { VirtualAssessmentController } from './virtual_assessment.controller';
 import { VirtualAssessmentService } from './virtual_assessment.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([VirtualAssessment, Appointment]),
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get('JWT_TOKEN_EXPIRE') },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([VirtualAssessment, Appointment])],
   providers: [VirtualAssessmentService],
   controllers: [VirtualAssessmentController],
 })
