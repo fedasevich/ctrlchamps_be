e7ab2203a96ace0aabaa5b5725cb3830cb0af131
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-12-22 17:31:46 +0200

    feat:add test for activity log

---
+++ src/modules/activity-log/activity-log.constants.ts
@@ -0,0 +1,7 @@
+import { ActivityLogStatus } from 'src/modules/activity-log/enums/activity-log-status.enum';
+
+export const ACTIVITY_LOG = {
+  appointmentId: '123e4567-e89b-12d3-a456-426614174000',
+  status: ActivityLogStatus.Approved,
+  tasks: ['Shopping grocery'],
+};
--- src/modules/activity-log/activity-log.controller.spec.ts
+++ src/modules/activity-log/activity-log.controller.spec.ts
@@ -1,15 +1,33 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { ACTIVITY_LOG } from 'src/modules/activity-log/activity-log.constants';
+import { TokenGuard } from 'src/modules/auth/middleware/auth.middleware';
 
 import { ActivityLogController } from './activity-log.controller';
 import { ActivityLogService } from './activity-log.service';
 
 describe('ActivityLogController', () => {
   let controller: ActivityLogController;
+  let mockActivityLogService;
+  let mockAuthGuard;
 
   beforeEach(async () => {
+    mockActivityLogService = {
+      create: jest.fn(() => ACTIVITY_LOG),
+    };
+
+    mockAuthGuard = {
+      canActivate: jest.fn(() => true),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ActivityLogController],
-      providers: [ActivityLogService],
+      providers: [
+        { provide: ActivityLogService, useValue: mockActivityLogService },
+        { provide: TokenGuard, useValue: mockAuthGuard },
+      ],
+      imports: [JwtModule.register({ secret: 'very_secret_key' })],
     }).compile();
 
     controller = module.get<ActivityLogController>(ActivityLogController);
@@ -17,5 +35,13 @@ describe('ActivityLogController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create an activity log', async () => {
+      await expect(controller.create(ACTIVITY_LOG)).resolves.not.toThrow();
+
+      expect(mockActivityLogService.create).toHaveBeenCalledWith(ACTIVITY_LOG);
+    });
   });
 });
