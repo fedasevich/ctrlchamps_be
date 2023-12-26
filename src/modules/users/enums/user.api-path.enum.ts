d3df02d7a5cb88dad9c23854ac799bd30982909c
Lilly <nuralievalilly17@gmail.com>
2023-12-26 14:59:28 +0200

    fix: fix routes

--- src/modules/users/enums/user.api-path.enum.ts
+++ src/modules/users/enums/user.api-path.enum.ts
@@ -1,5 +1,5 @@
 export enum UserApiPath {
   DetailedInfo = '/:userId',
-  UploadAvatar = '/uploadAvatar',
-  ChangePassword = '/changePassword/:userId',
+  UploadAvatar = '/upload-avatar',
+  ChangePassword = '/change-password/:userId',
 }
