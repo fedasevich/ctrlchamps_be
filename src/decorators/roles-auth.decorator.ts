579c066d8d9b36518cffcbb4ba6815521d9a80a5
Fedasevich <yurafedasevka@gmail.com>
2024-01-05 13:06:25 +0100

    feat: add role decorator to guard

---
+++ src/decorators/roles-auth.decorator.ts
@@ -0,0 +1,8 @@
+import { CustomDecorator, SetMetadata } from '@nestjs/common';
+
+import { UserRole } from 'src/modules/users/enums/user-role.enum';
+
+export const ALLOWED_ROLES_KEY = 'roles';
+
+export const AllowedRoles = (...roles: UserRole[]): CustomDecorator<string> =>
+  SetMetadata(ALLOWED_ROLES_KEY, roles);
