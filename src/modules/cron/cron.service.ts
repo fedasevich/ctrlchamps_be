7d650853d5abbf893671cf12e2b638b517cf1529
Lilly <nuralievalilly17@gmail.com>
2024-01-13 20:09:54 +0200

    feat: add notifications and emails for signing contracts

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
 
 import { NotificationMessage } from 'src/common/enums/notification-message.enum';
@@ -13,17 +14,30 @@ import {
   NEXT_DAY_NUMBER,
 } from 'src/modules/cron/cron.constants';
 
+import { EmailService } from '../email/services/email.service';
 import { NotificationService } from '../notification/notification.service';
 import { PaymentService } from '../payment/payment.service';
 import { VirtualAssessmentService } from '../virtual-assessment/virtual-assessment.service';
 
 @Injectable()
 export class CronService {
+  private readonly seekerVirtualAssessmentDoneTemplateId =
+    this.configService.get<string>(
+      'SENDGRID_SEEKER_SUBMIT_CONTRACT_PROPOSAL_TEMPLATE_ID',
+    );
+
+  private readonly caregiverVirtualAssessmentDoneTemplateId =
+    this.configService.get<string>(
+      'SENDGRID_CAREGIVER_SUBMIT_CONTRACT_PROPOSAL_TEMPLATE_ID',
+    );
+
   constructor(
+    private configService: ConfigService,
     private appointmentService: AppointmentService,
     private paymentService: PaymentService,
     private virtualAssessmentService: VirtualAssessmentService,
     private notificationService: NotificationService,
+    private emailService: EmailService,
   ) {}
 
   @Cron(EVERY_15_MINUTES)
@@ -36,6 +50,30 @@ export class CronService {
           virtualAssessment.appointment.id,
           { status: VirtualAssessmentStatus.Finished },
         );
+
+        this.notificationService.createNotification(
+          virtualAssessment.appointment.caregiverInfo.user.id,
+          virtualAssessment.appointment.id,
+          NotificationMessage.SignOff,
+          virtualAssessment.appointment.user.id,
+        );
+
+        this.notificationService.createNotification(
+          virtualAssessment.appointment.user.id,
+          virtualAssessment.appointment.id,
+          NotificationMessage.SignOff,
+          virtualAssessment.appointment.caregiverInfo.user.id,
+        );
+
+        this.emailService.sendEmail({
+          to: virtualAssessment.appointment.user.email,
+          templateId: this.seekerVirtualAssessmentDoneTemplateId,
+        });
+
+        this.emailService.sendEmail({
+          to: virtualAssessment.appointment.caregiverInfo.user.email,
+          templateId: this.caregiverVirtualAssessmentDoneTemplateId,
+        });
       }),
     );
   }
